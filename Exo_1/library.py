e74887443609049b538b6b3e81e403f136a1c4d5
Francois Ludwinski <luf3rt@bosch.com>
2025-04-22 10:28:19 +0200

    Add TP POO subject

---
+++ Exo_1/library.py
@@ -0,0 +1,22 @@
+class Person:
+    """To implement"""
+
+
+class Book:
+    """To implement"""
+
+
+class LibraryError(Exception):
+    """Base class for Library errors"""
+
+
+class Library:
+    """To implement."""
+
+
+def main():
+    """Test your code here"""
+
+
+if __name__ == "__main__":
+    main()
