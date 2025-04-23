c5ca3724c1db949a735df767ca771570dddd183f
Ulrich Orphée <djikpeo&gmail.com>
2025-04-23 09:25:54 +0200

    Add person class

--- Exo_1/library.py
+++ Exo_1/library.py
@@ -1,6 +1,14 @@
 class Person:
-    """To implement"""
+    """Class defining a person"""
+    def __init__(self, first_name: str, last_name: str, age: int, email: str, phone: str) -> None:
+        self.first_name = first_name.capitalize()
+        self.last_name = last_name.upper()
 
+    def __str__(self) -> str:
+        return f"{self.last_name} {self.first_name}"
+
+    def __repr__(self) -> str:
+        return f"{self.last_name} {self.first_name}"
 
 class Book:
     """To implement"""
